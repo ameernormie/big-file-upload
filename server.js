f5f22ee32059634098e089a10bbd3f5b051e26c4
Ameer Hamza <ameernormie@gmail.com>
2021-01-14 10:54:08 +0500

    refactor: clean react app code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import Upload from "./Upload";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Upload />
-    {/* <App/> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
